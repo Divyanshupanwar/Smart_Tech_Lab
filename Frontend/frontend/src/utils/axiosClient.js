7147d4b740612a9049acd56a25973ee0f176e907
Divyanshupanwar <divyanshupanwar32@gmail.com>
2026-04-29 18:11:35 +0530

    Refactor axiosClient to use VITE_API_BASE_URL directly

--- Frontend/frontend/src/utils/axiosClient.js
+++ Frontend/frontend/src/utils/axiosClient.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
-const apiBaseUrl = import.meta.env.VITE_API_BASE_URL
-  || (import.meta.env.PROD ? '/api' : 'http://localhost:3000');
-
 const axiosClient = axios.create({
-  baseURL: apiBaseUrl,
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json'
